17d9591ff23cb9a6277b7ac6a30607a9ee322edf
Trevor Norton <nortrevor02@gmail.com>
2025-03-31 00:19:04 -0500

    Added Tyler's Textbox

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/DressUp.tscn")
+	get_tree().change_scene_to_file("res://Scenes/textbox.tscn")
 
 
 func _on_settings_pressed() -> void:
