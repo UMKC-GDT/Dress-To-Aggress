7a4447023cf3776c69cf4b99be54e71758eae06e
Trevor Norton <nortrevor02@gmail.com>
2025-03-31 02:22:13 -0500

    Added Timer to Fight Scene

--- game_manager.gd
+++ game_manager.gd
@@ -1,18 +1,32 @@
 extends Node2D
 
 func _ready() -> void:
-	$RichTextLabel.text = ""
+	$Message.text = ""
+	$"Fight Timer Display".text = ""
+	
+func _process(delta: float) -> void:
+	$"Fight Timer Display".text = str($"Fight Timer Display/Fight Timer".time_left).pad_decimals(1)
 
 func _on_player_one_died() -> void:
-	print("Player One Died. Bozo.")
-	$RichTextLabel.text = "You Lose. Try Again!"
-	$RichTextLabel/Timer.start()
-	
+	$"../Player".visible = false
+	$"../Player2".visible = false
+	$Message.text = "You Lose. Try Again!"
+	$Message/Timer.start()
+	$"Fight Timer Display".visible = false
+
 func _on_cpu_died() -> void:
-	print("CPU Died. Nice Job!")
-	$RichTextLabel.text = "You Win! Congratulations, the villains have been stopped!"
-	$RichTextLabel/Timer.start()
+	$"../Player".visible = false
+	$"../Player2".visible = false
+	$Message.text = "You Win! Congratulations!"
+	$Message/Timer.start()
+	$"Fight Timer Display".visible = false
 
 func _on_timer_timeout() -> void:
 	var tree: SceneTree = get_tree()
 	tree.change_scene_to_file("res://Scenes/main_menu.tscn")
+
+
+func _on_fight_timer_timeout() -> void:
+	$Message.text = "Time Ran Out. Try Again!"
+	$Message/Timer.start()
+	$"Fight Timer Display".visible = false
