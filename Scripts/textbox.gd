005caaa4d42a57b36184a88efb16d5b44f036a1f
Zack <zckstfn@gmail.com>
2025-09-25 13:25:20 -0500

    Added delay to intro scene text and removed random letters

--- Scripts/textbox.gd
+++ Scripts/textbox.gd
@@ -2,19 +2,19 @@ extends Control
 
 var sentence = "*You are a secret agent, infiltrating an international drug smuggling ring. 
 \n*But it's all hidden... behind a fashion show? 
-\n*You're going to need your sharpest disguise - the fiercest outfit you can imagine.dkjfasdljflkasdjf"
+\n*You're going to need your sharpest disguise - the fiercest outfit you can imagine."
 
 var sen_index = 0
 
 func _on_timer_timeout() -> void:
-	if sen_index<sentence.length():
+	if sen_index < sentence.length():
 		$Panel/Label.text += sentence[sen_index]
-		sen_index+=1
+		sen_index += 1
 	else:
+		await get_tree().create_timer(1.5).timeout
 		change_scene()
 
 func change_scene() -> void:
-	#await get_tree().create_timer(1.5).timeout
 	print($AudioStreamPlayer.get_playback_position())
 	get_tree().change_scene_to_file("res://Scenes/DressUp.tscn")
 
