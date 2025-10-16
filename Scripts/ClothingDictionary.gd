4230260d3b6b61c47e225523a916185e849ddc63
CallmeCheese1 <tomiewilliams21@gmail.com>
2025-10-16 13:26:26 -0500

    Began Mockup of new Clothing Dictionary

---
+++ Scripts/ClothingDictionary.gd
@@ -0,0 +1,53 @@
+extends Node
+
+var clothing_items = {
+	"blueShorts": {
+		"type" : "short",
+		"color" : "#4597da",
+		"stats" : {
+			"atkDamage" : 0.0,
+			"atkSpeed" : -0.25,
+			"walkSpeed" : 0.0,
+			"dashSpeed" : 0.25,
+			"jumpHeight" : 0.0,
+			"atkHitstun" : 0.0,
+			"knockback" : 0.5,
+			"poseSpeed" : 0.0,
+			"poseHitstun" : 0.0,
+			"poseKnockback" : 0.0,
+			"poseDamage" : -0.125,
+			"health" : 0.0
+		}
+	},
+	
+	
+	"bluePants": "",
+	"purplePants": "",
+	"purpleShorts": "",
+	"whiteShorts": "",
+	"whitePants": "",
+	"blackPants": "",
+	"blackShorts": "",
+	"greenShorts": "",
+	"greenPants": "",
+	"redPants": "",
+	"redShorts": "",
+	"brownPants": "",
+	"brownShorts": "",
+	"grayShorts": "",
+	"blueShirtL": "",
+	"blueShirtS": "",
+	"purpleShirtS": "",
+	"purpleShirtL": "",
+	"whiteShirtL": "",
+	"whiteShirtS": "",
+	"blackShirtS": "",
+	"blackShirtL": "",
+	"greenShirtS": "",
+	"greenShirtL": "",
+	"redShirtS": "",
+	"redShirtL": "",
+	"brownShirtL": "",
+	"brownShirtS": "",
+	"grayShirtS": ""
+}
